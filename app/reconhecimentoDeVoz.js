254fdc422b2a1637d29f9ea183ef84ec154304c8
Wellington Ferreira <wellington_006@hotmail.com>
2023-08-18 17:18:34 -0300

    update

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,25 +1,24 @@
-const elementoChute = document.getElementById('chute');
+const elementoChute = document.getElementById('chute')
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
 recognition.lang = 'pt-Br'
-recognition.start();
+recognition.start()
 
-recognition.addEventListener('result', onSpeak);
+recognition.addEventListener('result', onSpeak)
 
-function onSpeak(evento){
-    chute = evento.results[0][0].transcript;
-    exibeChuteNaTela(chute);
-    verificaSeOChutePossuiUmValorValido(chute);
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
 }
 
-function exibeChuteNaTela(chute){
+function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
-    <div> Você disse</div>
-    <span class="box">${chute}</span>    
-    `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
 
-recognition.addEventListener("end", () => recognition.start());
+recognition.addEventListener('end', () => recognition.start())
